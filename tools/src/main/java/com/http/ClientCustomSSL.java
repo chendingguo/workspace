9e33b63130a37d7e47be6b6f529ce297b887449c
dingguo.chen <dingguochen@mtime.com>
2017-04-18 10:27:05 +0800

    rpc example

---
+++ tools/src/main/java/com/http/ClientCustomSSL.java
@@ -0,0 +1,48 @@
+package com.http;
+
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class ClientCustomSSL {
+
+    public final static void main(String[] args) throws Exception {
+        SSLContext sslcontext = SSLContext.getDefault();
+        // Allow TLSv1 protocol only
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
+                sslcontext,
+                new String[] { "TLSv1" },
+                null,
+                SSLConnectionSocketFactory.getDefaultHostnameVerifier());
+        CloseableHttpClient httpclient = HttpClients.custom()
+                .setSSLSocketFactory(sslsf)
+                .build();
+        try {
+
+            HttpGet httpget = new HttpGet("https://www.baidu.com/s?wd=姓名搜索定定定");
+            
+            System.out.println("Executing request " + httpget.getRequestLine());
+
+            CloseableHttpResponse response = httpclient.execute(httpget);
+            try {
+                HttpEntity entity = response.getEntity();
+
+                System.out.println("----------------------------------------");
+                System.out.println(response.getStatusLine());
+                System.out.println(EntityUtils.toString(response.getEntity(),"UTF-8"));
+                EntityUtils.consume(entity);
+            } finally {
+                response.close();
+            }
+        } finally {
+            httpclient.close();
+        }
+    }
+
+}
