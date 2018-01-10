962eccf1d19d5ff2e424f942a662153580497560
airsupply <cdg_mail@163.com>
2018-01-10 23:24:26 +0800

    add comment

--- rpc-master/src/test/java/com/nettyrpc/test/server/RpcBootstrap.java
+++ rpc-master/src/test/java/com/nettyrpc/test/server/RpcBootstrap.java
@@ -2,9 +2,13 @@ package com.nettyrpc.test.server;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ * start
+ */
 public class RpcBootstrap {
 
     public static void main(String[] args) {
+
         new ClassPathXmlApplicationContext("server-spring.xml");
     }
 }
